4d90b975509dd57182e6bb08f98b85306c32ad78
AKRAM <aiciakram18@gmail.com>
2024-10-04 11:21:45 +0100

    portfolio

---
+++ portfolio websites/scripts.js
@@ -0,0 +1,8 @@
+
+        var sidebar=document.getElementById("sidebar");
+        function openmenu(){
+            sidebar.style.right="0";
+        }
+        function closemenue(){
+            sidebar.style.right="-250px";
+        }
